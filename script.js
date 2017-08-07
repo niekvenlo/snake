8f789793067d201a609bb585d7bc488cd9c070e4
Nick Beaujean <nick@Nicks-MacBook-Pro.local>
2017-08-07 11:43:07 -0700

    use class notation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 window.onload = function() {
 
   // Intialize canvas
-  let cell_num = 15;
+  let cell_num = 15; // Grid size
   let cell_size = Math.floor((Math.min(window.innerHeight,window.innerWidth))/cell_num);
   let canvas_size = cell_size*cell_num;
   let canvas_mid = Math.floor(cell_num/2);
@@ -14,49 +14,43 @@ window.onload = function() {
   let context = canvas.getContext("2d");
   context.fillStyle = "#000";
 
-  // Snake game object literal
-  let snake = {
-    x: canvas_mid,
-    y: canvas_mid,
-    dir: 'down',
-    initialize() {
-      context.fillRect(snake.x*cell_size,snake.y*cell_size,cell_size,cell_size);
-    },
+  // Snake game class
+  class Snake {
+    constructor() {
+      this.x = canvas_mid;
+      this.y = canvas_mid;
+      this.dir = 'down';
+      context.fillRect(this.x*cell_size,this.y*cell_size,cell_size,cell_size);
+    }
     start() {
-      console.log('Starting game')
+      console.log('Starting game');
       this.interval = setInterval(this.step,1000);
-    },
+    }
     move() {
       switch (this.dir) {
-        case 'up':
-          this.y-=1;
-          break;
-        case 'down':
-          this.y+=1;
-          break;
-        case 'left':
-          this.x-=1;
-          break;
-        case 'right':
-          this.x+=1;
-          break;
+        case 'up':    this.y-=1; break;
+        case 'down':  this.y+=1; break;
+        case 'left':  this.x-=1; break;
+        case 'right': this.x+=1; break;
       }
       if (this.x < 0 || this.x > 14 || this.y < 0 || this.y > 14) {
         this.game_over();
       }
-    },
+    }
     step() {
       snake.move();
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.fillRect(snake.x*cell_size,snake.y*cell_size,cell_size,cell_size);
-    },
+    }
     game_over() {
       console.log('Game over');
       clearTimeout(this.interval);
-    },
+    }
   }
+
   // Initialize
-  snake.initialize();
+  snake = new Snake;
+  // snake.initialize();
 
   // Keyboard event listener
   document.addEventListener('keydown', keyHandler, false)
@@ -65,18 +59,10 @@ window.onload = function() {
       snake.start();
     };
     switch (event.code) {
-      case 'ArrowUp':
-        snake.dir = 'up';
-        break;
-      case 'ArrowRight':
-        snake.dir = 'right';
-        break;
-      case 'ArrowDown':
-        snake.dir = 'down';
-        break;
-      case 'ArrowLeft':
-        snake.dir = 'left';
-        break;
+      case 'ArrowUp':    snake.dir = 'up';    break;
+      case 'ArrowRight': snake.dir = 'right'; break;
+      case 'ArrowDown':  snake.dir = 'down';  break;
+      case 'ArrowLeft':  snake.dir = 'left';  break;
       default:
       // No action on other keys
     }
